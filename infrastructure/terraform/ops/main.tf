1e6bd57c312705a850b1da9ebc9ce8670f63de97
Thinh Nguyen <thinh.nguyen@codeleap.de>
2024-07-25 10:48:27 +0700

    feat: Add ops folder for Terraform

---
+++ infrastructure/terraform/ops/main.tf
@@ -0,0 +1,15 @@
+data "azurerm_resource_group" "rg" {
+  name = "${var.prefix}${var.env}rg"
+}
+module "storage_account" {
+  depends_on            = [data.azurerm_resource_group.rg]
+  source                = "../modules/storage-account"
+  prefix                = var.prefix
+  location              = var.location
+  env                   = var.env
+  rg_name               = data.azurerm_resource_group.rg
+  fs_name_debezium_conf = "conf"
+  fs_name_debezium_data = "data"
+  quota_debezium_conf   = 1
+  quota_debezium_data   = 50
+}
---
+++ infrastructure/terraform/ops/variables.tf
@@ -0,0 +1,5 @@
+variable "location" { type = string }
+
+variable "env" { type = string }
+
+variable "prefix" { type = string }
